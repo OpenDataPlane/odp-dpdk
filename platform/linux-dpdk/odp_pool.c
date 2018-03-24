609e72bc7eeaa9193b32d1b8f649bcf65071e465
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2018-03-24 04:54:11 +0300

    linux-dpdk: port to DPDK 17.11
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>

--- platform/linux-dpdk/odp_pool.c
+++ platform/linux-dpdk/odp_pool.c
@@ -31,6 +31,7 @@
 
 /* for DPDK */
 #include <odp_packet_dpdk.h>
+#include <rte_version.h>
 
 #ifdef POOL_USE_TICKETLOCK
 #include <odp/api/ticketlock.h>
@@ -205,8 +206,13 @@ odp_dpdk_mbuf_ctor(struct rte_mempool *mp,
 	/* Start of buffer is just after the ODP type specific header
 	 * which contains in the very beginning the rte_mbuf struct */
 	mb->buf_addr     = (char *)mb + mb_ctor_arg->seg_buf_offset;
+#if RTE_VERSION < RTE_VERSION_NUM(17, 11, 0, 0)
 	mb->buf_physaddr = rte_mempool_virt2phy(mp, mb) +
 			mb_ctor_arg->seg_buf_offset;
+#else
+	mb->buf_physaddr = rte_mempool_virt2iova(mb) +
+			mb_ctor_arg->seg_buf_offset;
+#endif
 	mb->buf_len      = mb_ctor_arg->seg_buf_size;
 	mb->priv_size = rte_pktmbuf_priv_size(mp);
 
