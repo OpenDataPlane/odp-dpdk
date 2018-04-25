0eaa26a5ea7391d79617f1a5933ccaaedf03257d
Matias Elo <matias.elo@nokia.com>
2018-04-25 10:26:14 +0300

    Port 2c99a9d "linux-gen: packet: IPv4 checksum insertion"
    Signed-off-by: Matias Elo <matias.elo@nokia.com>

--- platform/linux-dpdk/include/odp_packet_internal.h
+++ platform/linux-dpdk/include/odp_packet_internal.h
@@ -261,6 +261,8 @@ int _odp_packet_set_data(odp_packet_t pkt, uint32_t offset,
 int _odp_packet_cmp_data(odp_packet_t pkt, uint32_t offset,
 			 const void *s, uint32_t len);
 
+int _odp_packet_ipv4_chksum_insert(odp_packet_t pkt);
+
 /* We can't enforce tailroom reservation for received packets */
 ODP_STATIC_ASSERT(CONFIG_PACKET_TAILROOM == 0,
 		  "ERROR: Tailroom has to be 0, DPDK doesn't support this");
