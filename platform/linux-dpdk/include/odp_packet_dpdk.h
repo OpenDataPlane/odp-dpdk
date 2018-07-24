ec63646a8149c9fc761d840b98a8f2903f2368a1
Matias Elo <matias.elo@nokia.com>
2018-07-24 13:47:58 +0300

    linux-dpdk: packet: use inlined odp_packet_len() internally
    No need to use internal packet_len() function.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-dpdk/include/odp_packet_dpdk.h
+++ platform/linux-dpdk/include/odp_packet_dpdk.h
@@ -8,6 +8,7 @@
 #define ODP_PACKET_DPDK_H_
 
 #include <odp/api/packet_io.h>
+#include <odp/api/plat/packet_inlines.h>
 #include <odp_packet_internal.h>
 
 #include <stdint.h>
@@ -26,9 +27,10 @@ static inline int dpdk_packet_parse_layer(odp_packet_hdr_t *pkt_hdr,
 					  odp_pktio_parser_layer_t layer,
 					  odp_pktin_config_opt_t pktin_cfg)
 {
-	uint32_t seg_len = odp_packet_seg_len(packet_handle(pkt_hdr));
-	uint32_t len = packet_len(pkt_hdr);
-	uint8_t *base = odp_packet_data(packet_handle(pkt_hdr));
+	odp_packet_t pkt = packet_handle(pkt_hdr);
+	uint32_t seg_len = odp_packet_seg_len(pkt);
+	uint32_t len = odp_packet_len(pkt);
+	uint8_t *base = odp_packet_data(pkt);
 
 	return dpdk_packet_parse_common(&pkt_hdr->p, base, len,
 					seg_len, mbuf, layer, pktin_cfg);
