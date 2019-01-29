96e69fc3f9ac693b9f2a32904967daddfb012fc7
Matias Elo <matias.elo@nokia.com>
2019-01-29 11:13:55 +0200

    Port 3a9cf94b7 "linux-gen: fix compiling dpdk pktio with Clang"
    Signed-off-by: Matias Elo <matias.elo@nokia.com>

--- scripts/ci/build_x86_64.sh
+++ scripts/ci/build_x86_64.sh
@@ -4,6 +4,7 @@ set -e
 export TARGET_ARCH=x86_64-linux-gnu
 if [ "${CC#clang}" != "${CC}" ] ; then
 	export CXX="clang++"
+	sed -i 's/ODP_CHECK_CFLAG(\[\-Wcast-align\])/#ODP_CHECK_CFLAG(\[\-Wcast-align\])/g' /odp/configure.ac
 fi
 
 exec "$(dirname "$0")"/build.sh
