01ea2dd8a2382007504e71eddc13f1f74ea18bfb
Matias Elo <matias.elo@nokia.com>
2020-09-15 17:00:32 +0300

    linux-dpdk: change ODP library name to libodp-dpdk
    Change ODP-DPDK platform library name to libodp-dpdk to better separate
    different ODP platforms.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -20,9 +20,14 @@ if ! [ -x "$(command -v ${PKG_CONFIG})" ]; then
         PKG_CONFIG="pkg-config"
 fi
 
+# Default ODP library name
+if [ -z "$ODP_LIB_NAME" ] ; then
+ODP_LIB_NAME=libodp-dpdk
+fi
+
 CC="${CC:-${TARGET_ARCH}-gcc}"
 ${CC} ${CFLAGS} ${OLDPWD}/example/hello/odp_hello.c -o odp_hello_inst_dynamic \
-	`PKG_CONFIG_PATH=/opt/odp/lib/pkgconfig ${PKG_CONFIG} --cflags --libs libodp-linux` \
+	`PKG_CONFIG_PATH=/opt/odp/lib/pkgconfig ${PKG_CONFIG} --cflags --libs ${ODP_LIB_NAME}` \
 	`${PKG_CONFIG} --cflags --libs libdpdk`
 
 sysctl vm.nr_hugepages=1000
