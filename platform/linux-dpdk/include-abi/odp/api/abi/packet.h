4e4ff7f17dfd0bb5fb39143e18bc5667817ca88c
Matias Elo <matias.elo@nokia.com>
2020-12-21 11:31:08 +0200

    Port 527561e9a "api: event: introduce packet vector event"
    Port original commit from linux-generic.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

--- platform/linux-dpdk/include-abi/odp/api/abi/packet.h
+++ platform/linux-dpdk/include-abi/odp/api/abi/packet.h
@@ -34,6 +34,10 @@ typedef ODP_HANDLE_T(odp_packet_seg_t);
 
 #define ODP_PACKET_SEG_INVALID _odp_cast_scalar(odp_packet_seg_t, 0)
 
+typedef ODP_HANDLE_T(odp_packet_vector_t);
+
+#define ODP_PACKET_VECTOR_INVALID _odp_cast_scalar(odp_packet_vector_t, 0)
+
 typedef uint8_t odp_proto_l2_type_t;
 
 #define ODP_PROTO_L2_TYPE_NONE   0
