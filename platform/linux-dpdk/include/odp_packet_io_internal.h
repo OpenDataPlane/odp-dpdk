9fd4266bdbfb2c8e9fa9f0d279a9baed9dccf8a1
Matias Elo <matias.elo@nokia.com>
2021-03-04 10:28:20 +0200

    Port e1afee9e0 "linux-gen: pktio: implement LSO API"
    Port original commit from linux-generic.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

--- platform/linux-dpdk/include/odp_packet_io_internal.h
+++ platform/linux-dpdk/include/odp_packet_io_internal.h
@@ -34,6 +34,11 @@ extern "C" {
 #include <sys/select.h>
 
 #define PKTIO_MAX_QUEUES 64
+#define PKTIO_LSO_PROFILES 16
+#define PKTIO_LSO_MAX_PAYLOAD_OFFSET 128
+#define PKTIO_LSO_MAX_SEGMENTS 8
+ODP_STATIC_ASSERT(PKTIO_LSO_PROFILES < UINT8_MAX, "PKTIO_LSO_PROFILES_ERROR");
+
 #define PKTIO_NAME_LEN 256
 
 #define PKTIN_INVALID  ((odp_pktin_queue_t) {ODP_PKTIO_INVALID, 0})
@@ -130,6 +135,13 @@ typedef union {
 	uint8_t pad[ROUNDUP_CACHE_LINE(sizeof(struct pktio_entry))];
 } pktio_entry_t;
 
+typedef struct {
+	odp_lso_profile_param_t param;
+	int used;
+	uint8_t index;
+
+} lso_profile_t;
+
 /* Global variables */
 typedef struct {
 	odp_spinlock_t lock;
@@ -141,6 +153,10 @@ typedef struct {
 	} config;
 
 	pktio_entry_t  entries[ODP_CONFIG_PKTIO_ENTRIES];
+
+	lso_profile_t lso_profile[PKTIO_LSO_PROFILES];
+	int num_lso_profiles;
+
 } pktio_global_t;
 
 typedef struct pktio_if_ops {
