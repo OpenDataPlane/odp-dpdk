OpenDataPlane/odp-dpdk

commit cf4d3329d4a9b53663ee27a904ee88f2cd3f50d8

linux-dpdk: crypto: disable AES-CMAC in aesni_mb crypto driver
Having the IPsec module use AES-CMAC implementation provided by the
aesni_mb crypto driver crashes validation tests inside the intel MB
library. The root cause is not yet known. As a workaroud, do not
advertise AES-CMAC capability of the aesni_mb driver.

Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
Reviewed-by: Matias Elo <matias.elo@nokia.com>

platform/linux-dpdk/odp_crypto.c
@@ -575,7 +575,14 @@ static void capability_process(struct rte_cryptodev_info *dev_info,
 				auths->bit.sha512_hmac = 1;
 			if (cap_auth_algo == RTE_CRYPTO_AUTH_AES_GMAC)
 				auths->bit.aes_gmac = 1;
-			if (cap_auth_algo == RTE_CRYPTO_AUTH_AES_CMAC)
+
+			/* Using AES-CMAC with the aesni_mb driver for IPsec
+			 * causes a crash inside the intel-mb library.
+			 * As a workaround, we do not use AES-CMAC with
+			 * the aesni_mb driver.
+			 */
+			if (cap_auth_algo == RTE_CRYPTO_AUTH_AES_CMAC &&
+			    !is_dev_aesni_mb(dev_info))
 				auths->bit.aes_cmac = 1;
 
 			/* Combination of (3)DES-CBC and AES-XCBC-MAC does not

commit 62780a462d28189132e0a1eaaa6dff9b0e33abee

linux-dpdk: crypto: fix disabling of AES-CMAC with aesni-mb driver
AES-CMAC is not included in crypto capabilities when found in aesni-mb
but it may still be included because some other crypto device supports it.
If aesni-mb device and another device that does support AES-CMAC are
present at the same time, then the aesni-mb device may still get selected
for a session that uses AES-CMAC.

Fix device selection in session creation by excluding AES-CMAC sessions
from the aesni-mb device.

Fixes: cf4d3329d4a9b53663ee27a904ee88f2cd3f50d8

Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
Reviewed-by: Matias Elo <matias.elo@nokia.com>

platform/linux-dpdk/odp_crypto.c
@@ -1216,6 +1216,13 @@ check_auth:
 		if (cap->op == RTE_CRYPTO_OP_TYPE_UNDEFINED)
 			continue;
 
+		/* As a bug workaround, we do not use AES_CMAC with
+		 * the aesni-mb crypto driver.
+		 */
+		if (auth_xform->auth.algo == RTE_CRYPTO_AUTH_AES_CMAC &&
+		    is_dev_aesni_mb(&dev_info))
+			continue;
+
 		/* As a bug workaround, we do not use AES_XCBC_MAC with
 		 * the aesni-mb crypto driver.
 		 */