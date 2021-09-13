e79f643c9c4bedcca9674c8ad223a91392fdf9fc
Matias Elo <matias.elo@nokia.com>
2021-09-13 13:19:14 +0300

    linux-dpdk: align: use RTE_CACHE_LINE_SIZE as ODP_CACHE_LINE_SIZE
    Define ODP_CACHE_LINE_SIZE using RTE_CACHE_LINE_SIZE. Previously, the
    values could differ due to configuration differences.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

--- platform/linux-dpdk/arch/x86/odp/api/abi/cpu.h
+++ platform/linux-dpdk/arch/x86/odp/api/abi/cpu.h
@@ -1,1 +1,26 @@
-../../../../../../linux-generic/arch/x86/odp/api/abi/cpu.h
+/* Copyright (c) 2016-2018, Linaro Limited
+ * Copyright (c) 2021, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#ifndef ODP_API_ABI_CPU_H_
+#define ODP_API_ABI_CPU_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <rte_common.h>
+
+#define ODP_CACHE_LINE_SIZE RTE_CACHE_LINE_SIZE
+
+/* Inlined functions for non-ABI compat mode */
+#include <odp/api/plat/cpu_inlines.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
