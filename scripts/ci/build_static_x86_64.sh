fb0490b3945e7a36c0dea458aee42b51731dc643
Matias Elo <matias.elo@nokia.com>
2022-10-12 09:18:49 +0300

    ci: enable usage of static linking test script
    Enable running static linking test script with other ODP implementations
    than linux-generic by removing the hard coded library name. Huge page
    mappings are also added.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

--- scripts/ci/build_static_x86_64.sh
+++ scripts/ci/build_static_x86_64.sh
@@ -17,8 +17,19 @@ make install
 # Build and run sysinfo with installed libs
 pushd ${HOME}
 
-${CC} ${CFLAGS} ${OLDPWD}/example/sysinfo/odp_sysinfo.c -static -o odp_sysinfo_inst_static `PKG_CONFIG_PATH=/opt/odp/lib/pkgconfig:${PKG_CONFIG_PATH} pkg-config --cflags --libs --static libodp-linux`
+# Default ODP library name
+if [ -z "$ODP_LIB_NAME" ] ; then
+ODP_LIB_NAME=libodp-dpdk
+fi
+
+${CC} ${CFLAGS} ${OLDPWD}/example/sysinfo/odp_sysinfo.c -static -o odp_sysinfo_inst_static `PKG_CONFIG_PATH=/opt/odp/lib/pkgconfig:${PKG_CONFIG_PATH} pkg-config --cflags --libs --static ${ODP_LIB_NAME}`
+
+echo 1500 | tee /proc/sys/vm/nr_hugepages
+mkdir -p /mnt/huge
+mount -t hugetlbfs nodev /mnt/huge
 
 ./odp_sysinfo_inst_static
+
+umount /mnt/huge
 
 popd
