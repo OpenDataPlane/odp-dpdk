74c616400f239d2adedf1929568875874b8e0000
Matias Elo <matias.elo@nokia.com>
2022-11-07 10:07:39 +0200

    Port d77b8b6fe "linux-gen: packet: fix odp_packet_parse() with nonzero offset"
    Port original commit from linux-generic.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

--- platform/linux-dpdk/odp_packet.c
+++ platform/linux-dpdk/odp_packet.c
@@ -1499,6 +1499,7 @@ int odp_packet_parse(odp_packet_t pkt, uint32_t offset,
 	odp_packet_hdr_t *pkt_hdr = packet_hdr(pkt);
 	const uint8_t *data;
 	uint32_t seg_len;
+	uint32_t seg_end;
 	uint32_t packet_len = odp_packet_len(pkt);
 	odp_proto_t proto = param->proto;
 	odp_proto_layer_t layer = param->last_layer;
@@ -1531,6 +1532,8 @@ int odp_packet_parse(odp_packet_t pkt, uint32_t offset,
 		data = buf;
 	}
 
+	seg_end = offset + seg_len; /* one past the maximum offset */
+
 	/* Reset parser flags, keep other flags */
 	packet_parse_reset(pkt_hdr, 0);
 
@@ -1554,7 +1557,7 @@ int odp_packet_parse(odp_packet_t pkt, uint32_t offset,
 	opt.bit.sctp_chksum = param->chksums.chksum.sctp;
 
 	ret = _odp_packet_parse_common_l3_l4(&pkt_hdr->p, data, offset,
-					     packet_len, seg_len, layer,
+					     packet_len, seg_end, layer,
 					     ethtype, &l4_part_sum, opt);
 
 	if (ret)
