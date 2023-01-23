e92112f79ea18f7148b418320ec3215d259e0794
Matias Elo <matias.elo@nokia.com>
2023-01-23 10:56:26 +0200

    Port 76e996322 "linux-gen: crypto: inline odp_crypto_result() implementation"
    Port original commit from linux-generic.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>

--- platform/linux-dpdk/odp_packet.c
+++ platform/linux-dpdk/odp_packet.c
@@ -1,5 +1,5 @@
 /* Copyright (c) 2013-2018, Linaro Limited
- * Copyright (c) 2019-2022, Nokia
+ * Copyright (c) 2019-2023, Nokia
  * All rights reserved.
  *
  * SPDX-License-Identifier:     BSD-3-Clause
@@ -66,6 +66,7 @@ const _odp_packet_inline_offset_t _odp_packet_inline ODP_ALIGNED_CACHE = {
 	.subtype          = offsetof(odp_packet_hdr_t, subtype),
 	.cls_mark         = offsetof(odp_packet_hdr_t, cls_mark),
 	.ipsec_ctx        = offsetof(odp_packet_hdr_t, ipsec_ctx),
+	.crypto_op        = offsetof(odp_packet_hdr_t, crypto_op_result),
 	.buf_addr         = offsetof(odp_packet_hdr_t, event_hdr.mb.buf_addr),
 	.data             = offsetof(odp_packet_hdr_t, event_hdr.mb.data_off),
 	.pkt_len          = offsetof(odp_packet_hdr_t, event_hdr.mb.pkt_len),
