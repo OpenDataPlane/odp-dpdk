355947377b57a34d7ebe706c9c8346f65c9984c2
Matias Elo <matias.elo@nokia.com>
2024-05-07 15:43:42 +0300

    linux-dpdk: pool: take alignment into account in buf.max_size capability
    Include default data alignment when calculating
    odp_pool_capability_t.buf.max_size capability.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>

--- platform/linux-dpdk/odp_pool.c
+++ platform/linux-dpdk/odp_pool.c
@@ -56,8 +56,9 @@
 /* Pool name format */
 #define POOL_NAME_FORMAT "%" PRIu64 "-%d-%s"
 
-/* Define a practical limit for contiguous memory allocations */
-#define MAX_SIZE   (CONFIG_PACKET_SEG_SIZE - CONFIG_BUFFER_ALIGN_MIN)
+/* Default alignment for buffers */
+#define BUFFER_ALIGN_DEFAULT ODP_CACHE_LINE_SIZE
+ODP_STATIC_ASSERT(_ODP_CHECK_IS_POWER2(BUFFER_ALIGN_DEFAULT), "Buffer align must be power of two");
 
 /* Maximum packet user area size */
 #define MAX_UAREA_SIZE 2048
@@ -262,7 +263,7 @@ int odp_pool_capability(odp_pool_capability_t *capa)
 	/* Buffer pools */
 	capa->buf.max_pools = max_pools;
 	capa->buf.max_align = CONFIG_BUFFER_ALIGN_MAX;
-	capa->buf.max_size  = MAX_SIZE;
+	capa->buf.max_size  = _ODP_ROUNDDOWN_POWER2(CONFIG_PACKET_SEG_SIZE, BUFFER_ALIGN_DEFAULT);
 	capa->buf.max_num   = CONFIG_POOL_MAX_NUM;
 	capa->buf.max_uarea_size   = MAX_UAREA_SIZE;
 	capa->buf.uarea_persistence = true;
@@ -683,7 +684,7 @@ odp_pool_t _odp_pool_create(const char *name, const odp_pool_param_t *params,
 		num = params->buf.num;
 		cache_size = params->buf.cache_size;
 		priv_size = get_mbuf_priv_size(sizeof(odp_buffer_hdr_t));
-		align = params->buf.align > 0 ? params->buf.align : ODP_CACHE_LINE_SIZE;
+		align = params->buf.align > 0 ? params->buf.align : BUFFER_ALIGN_DEFAULT;
 		align = _ODP_MAX(align, (uint32_t)CONFIG_BUFFER_ALIGN_MIN);
 		data_size = _ODP_ROUNDUP_ALIGN(params->buf.size + trailer, align);
 		uarea_size = params->buf.uarea_size;
@@ -1117,7 +1118,7 @@ int odp_pool_ext_capability(odp_pool_type_t type,
 	capa->stats.all = supported_stats.all;
 
 	capa->pkt.max_num_buf = _odp_pool_glb->config.pkt_max_num;
-	capa->pkt.max_buf_size = MAX_SIZE;
+	capa->pkt.max_buf_size = CONFIG_PACKET_SEG_SIZE - CONFIG_BUFFER_ALIGN_MIN;
 	capa->pkt.odp_header_size = SIZEOF_OBJHDR + sizeof(odp_packet_hdr_t);
 	capa->pkt.odp_trailer_size = _ODP_EV_ENDMARK_SIZE;
 	capa->pkt.min_mem_align = ODP_CACHE_LINE_SIZE;
