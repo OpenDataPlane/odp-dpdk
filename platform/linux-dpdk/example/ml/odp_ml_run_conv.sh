05a1cd6695442ed6ea619a4ff987688cae8e64bb
Jere Leppänen <jere.leppanen@nokia.com>
2024-09-19 14:05:49 +0300

    linux-dpdk: example: ml: include in build
    Include linux-dpdk/example/ml in build. It was removed in a recent
    revert along with the onnx model based examples. The conv example is
    skipped in make check, unless the model file is manually provided.
    
    Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>
    Reviewed-by: Lifang Zhang <lifang.zhang@nokia.com>

--- platform/linux-dpdk/example/ml/odp_ml_run_conv.sh
+++ platform/linux-dpdk/example/ml/odp_ml_run_conv.sh
@@ -1,1 +1,28 @@
-../../../linux-generic/example/ml/odp_ml_run_conv.sh
+#!/bin/sh
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 Nokia
+
+TEST_DIR="${TEST_DIR:-$(dirname $0)}"
+MODEL_FILE=$TEST_DIR/conv.bin
+
+# exit codes expected by automake for skipped tests
+TEST_SKIPPED=77
+
+# Skip test if model file is not found. The model file is compiled separately.
+# See platform/linux-dpdk/example/ml/conv_gen.sh.
+
+if [ ! -e "$MODEL_FILE" ]; then
+        echo "SKIP: ML model file ($MODEL_FILE) not found"
+        exit $TEST_SKIPPED
+fi
+
+../../../../example/ml/odp_ml_run${EXEEXT} \
+                -m $MODEL_FILE -i conv-input.bin -r conv-output.bin -q 1 -d 1
+
+if [ $? -ne 0 ] ; then
+    echo Test FAILED
+    exit 1
+fi
+
+exit 0
