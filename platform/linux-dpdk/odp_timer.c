03eeaca9caf0f2bcb98776cbc72487df8c1a8ebb
Matias Elo <matias.elo@nokia.com>
2025-06-16 12:27:02 +0300

    Port e521decdb "linux-gen: timer: do not log an error in capability query"
    Port original commit from linux-generic.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>

--- platform/linux-dpdk/odp_timer.c
+++ platform/linux-dpdk/odp_timer.c
@@ -483,7 +483,7 @@ int odp_timer_capability(odp_timer_clk_src_t clk_src,
 	uint64_t min_tmo = tmo_ticks_to_ns_round_up(MIN_TMO_CYCLES);
 
 	if (clk_src != ODP_CLOCK_DEFAULT) {
-		_ODP_ERR("Only ODP_CLOCK_DEFAULT supported. Requested %i.\n", clk_src);
+		_ODP_DBG("Only ODP_CLOCK_DEFAULT supported. Requested %i.\n", clk_src);
 		return -1;
 	}
 
