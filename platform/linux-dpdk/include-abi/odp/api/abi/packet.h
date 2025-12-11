fc09401a888b3e3dfa39dddb9460e28299f49775
Matias Elo <matias.elo@nokia.com>
2025-12-11 10:47:29 +0200

    Port 89d5e1993 "api: packet: move odp_packet_parse_result_flag_t definition to API"
    Port original commit from linux-generic.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Janne Peltonen <janne.peltonen@nokia.com>

--- platform/linux-dpdk/include-abi/odp/api/abi/packet.h
+++ platform/linux-dpdk/include-abi/odp/api/abi/packet.h
@@ -1,17 +1,1 @@
-/* SPDX-License-Identifier: BSD-3-Clause
- * Copyright (c) 2015-2018 Linaro Limited
- */
-
-/**
- * @file
- *
- * ODP packet descriptor
- */
-
-#ifndef ODP_API_ABI_PACKET_H_
-#define ODP_API_ABI_PACKET_H_
-
-#include <odp/api/plat/packet_inlines.h>
-#include <odp/api/plat/packet_vector_inlines.h>
-
-#endif
+../../../../../linux-generic/include-abi/odp/api/abi/packet.h
