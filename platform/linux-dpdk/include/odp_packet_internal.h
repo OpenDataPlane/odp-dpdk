2b15b2b13ba4c87beb7d1986406d95f3e1e602df
Matias Elo <matias.elo@nokia.com>
2026-07-02 14:29:54 +0300

    Port b7580e9b8 "linux-gen: packet: fix TX aging timeout and TX completion ID metadata copying"
    Port original commit from linux-generic.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Janne Peltonen <janne.peltonen@nokia.com>

--- platform/linux-dpdk/include/odp_packet_internal.h
+++ platform/linux-dpdk/include/odp_packet_internal.h
@@ -320,6 +320,12 @@ static inline void _odp_packet_copy_md(odp_packet_hdr_t *dst_hdr,
 	if (src_hdr->p.flags.payload_off)
 		dst_hdr->payload_offset = src_hdr->payload_offset;
 
+	if (src_hdr->p.flags.tx_aging)
+		dst_hdr->tx_aging_ns = src_hdr->tx_aging_ns;
+
+	if (src_hdr->p.flags.tx_compl_poll)
+		dst_hdr->tx_compl_id = src_hdr->tx_compl_id;
+
 	dst_hdr->p = src_hdr->p;
 
 	if (src_hdr->uarea_addr) {
